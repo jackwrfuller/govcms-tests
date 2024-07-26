77ff4c56e500cb50b53766c7f57417ce9b85a7be
Joseph Zhao <pandaski@outlook.com.au>
2024-07-26 12:37:09 +1000

    Add more configs

---
+++ .docker/sso/config/metadata/saml20-idp-remote.php
@@ -0,0 +1,9 @@
+$metadata['http://localhost:8080/simplesaml/saml2/idp/metadata.php'] = array(
+    'name' => array(
+        'en' => 'Test IdP',
+    ),
+    'description' => 'Test IdP',
+    'SingleSignOnService' => 'http://localhost:8080/simplesaml/saml2/idp/SSOService.php',
+    'SingleLogoutService' => 'http://localhost:8080/simplesaml/saml2/idp/SingleLogoutService.php',
+    'certFingerprint' => '119b9e027959cdb7c662cfd075d9e2ef384e445f',
+);
